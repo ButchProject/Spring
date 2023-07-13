7aa602fac42277bd92712a241b1028f8606b0b4b
GemGemMin <65663148+GemGemMin@users.noreply.github.com>
2023-07-13 15:09:59 +0900

    add list, detail

--- src/main/java/codingrecipe/member/controller/MemberController.java
+++ src/main/java/codingrecipe/member/controller/MemberController.java
@@ -5,9 +5,13 @@ import codingrecipe.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor // lombok에서 제공
@@ -29,30 +33,47 @@ public class MemberController {
         return null;
     }*/
     @PostMapping("/member/save")
-    public String save(@ModelAttribute MemberDTO memberDTO){
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
 
         return "login";
     }
+
     @GetMapping("/member/login")
-    public String loginForm(){return "login";}
+    public String loginForm() {
+        return "login";
+    }
 
     @PostMapping("/member/login")
     public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         MemberDTO loginResult = memberService.login(memberDTO);
-        if(loginResult != null){
+        if (loginResult != null) {
             // login 성공
             session.setAttribute("loginEmail", loginResult.getMemberEmail());
             // 세션 기록
             return "main";
-        }
-        else {
+        } else {
             // login 실패
             return "login";
         }
     }
 
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // html로 가져갈 데이터가 있다면 model을 많이 사용함.
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
+    }
 
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        // 경로상의 값을 가져올 때 @PathVariable을 많이 사용함.
+        // id에 해당하는 객체를 DB에서 가져와서 화면에 뿌려야하기 때문에 Model 객체가 필요함.
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "detail";
+    }
 }
