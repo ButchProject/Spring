bdc50e9b851e83c95c150407c186531c8305cc99
GemGemMin <65663148+GemGemMin@users.noreply.github.com>
2023-07-13 18:36:56 +0900

    add modify_profile

--- src/main/java/codingrecipe/member/entity/MemberEntity.java
+++ src/main/java/codingrecipe/member/entity/MemberEntity.java
@@ -39,4 +39,15 @@ public class MemberEntity {
         memberEntity.setAcademyName(memberDTO.getAcademyName());
         return memberEntity;
     }
+
+    public static MemberEntity toUpdateMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setId(memberDTO.getId());
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setPhoneNumber(memberDTO.getPhoneNumber());
+        memberEntity.setAcademyName(memberDTO.getAcademyName());
+        return memberEntity;
+    }
 }
