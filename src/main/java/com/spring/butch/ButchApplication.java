097dbdd7192f4e2c834d30b75c3fc25845ec12b3
GemGemMin <65663148+GemGemMin@users.noreply.github.com>
2023-08-10 17:51:08 +0900

    Initial commit

---
+++ src/main/java/com/spring/butch/ButchApplication.java
@@ -0,0 +1,13 @@
+package com.spring.butch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/butch/ButchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.butch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
