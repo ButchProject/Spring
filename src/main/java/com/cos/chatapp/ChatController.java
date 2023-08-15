0859c6a70147372c2c96a338d6fa8531ba45fec3
GemGemMin <65663148+GemGemMin@users.noreply.github.com>
2023-08-15 17:15:31 +0900

    Initial commit

---
+++ src/main/java/com/cos/chatapp/ChatController.java
@@ -0,0 +1,32 @@
+package com.cos.chatapp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class ChatController {
+    private final ChatRepository chatRepository;
+
+    @CrossOrigin
+    @GetMapping(value = "sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMessage(@PathVariable String sender, @PathVariable String receiver){
+        return chatRepository.mFindBySender(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat){
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat); // object를 리턴하면 자동으로 JSON으로 바꿔줌 (MessageConverter)
+    }
+}
