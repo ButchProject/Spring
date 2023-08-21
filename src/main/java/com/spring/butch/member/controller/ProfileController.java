6ff73a45ddd91e856506cc9649a25e6b9582457c
GemGemMin <65663148+GemGemMin@users.noreply.github.com>
2023-08-21 17:31:33 +0900

    add ProfileController

--- src/main/java/com/spring/butch/member/controller/ProfileController.java
+++ src/main/java/com/spring/butch/member/controller/ProfileController.java
@@ -4,10 +4,7 @@ import com.spring.butch.member.dto.MemberDTO;
 import com.spring.butch.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profile")
@@ -15,8 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfileController {
 
     private final MemberService memberService;
-    @PostMapping("/edit")
-    public ResponseEntity<Void> update(@ModelAttribute MemberDTO memberDTO) {
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberDTO> findById(@PathVariable Long id) {
+        // 뒤에 {id}위치에 쿼리 파라미터로 주면 한 명의 회원 정보만을 받을 수 있음
+        MemberDTO memberDTO = memberService.findById(id);
+        if (memberDTO != null) {
+            return ResponseEntity.ok(memberDTO);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+
+    @PostMapping("/{id}/edit")
+    public ResponseEntity<Void> update(@PathVariable Long id, @ModelAttribute MemberDTO memberDTO) {
         memberService.update(memberDTO);
         return ResponseEntity.ok().build();
     }
