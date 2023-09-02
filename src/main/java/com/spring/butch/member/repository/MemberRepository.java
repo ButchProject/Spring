03c488c1fc80a08c239a3dd67d58a7069b53a8f9
GemGemMin <65663148+GemGemMin@users.noreply.github.com>
2023-09-02 15:25:34 +0900

    merge member branch

---
+++ src/main/java/com/spring/butch/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.spring.butch.member.repository;
+
+import com.spring.butch.member.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface  MemberRepository extends JpaRepository<MemberEntity, Long> {
+    //이메일로 회원 정보 조회 (select * from member_table where member_email=?)
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
+    // optional은 null방지 목적으로 사용
+
+} // Entity 클래스의 이름과 PK의 형태를 넘겨줘야 함.
